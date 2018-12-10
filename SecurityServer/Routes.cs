e84386fe08e7d8481da13e692d8e78e8534dd54d
FengZhanyuan <Zhanyuan.Feng@partner.bmw.com>
2018-12-10 15:25:14 +0800

    ddd

---
+++ SecurityServer/Routes.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecurityServer
+{
+    public static class Routes
+    {
+        public static IApplicationBuilder UseRoute(this IApplicationBuilder builder)
+        {
+
+            builder.Map("/user", app =>
+            {
+                app.Run(async (context) =>
+                {
+                    await context.Response.WriteAsync("Hello User!");
+                });
+            });
+
+
+
+
+
+
+            return builder;
+        }
+    }
+}
